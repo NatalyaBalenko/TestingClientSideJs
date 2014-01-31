7ba1259de83f3fa463627176f2af6be49ceba190
jain208 <abhi2000@gmail.com>
2014-01-31 16:09:26 +0000

    setup for Jasmine tests

---
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/spec/MySpecs.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../lib/jasmine-2.0.0/jasmine.js" />
+/// <reference path="../src/App.js" />
+
+describe("App", function () {
+    describe("UI", function () {
+
+    });
+
+    describe("Math", function () {
+
+    });
+
+});
---
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/src/App.js
@@ -0,0 +1,19 @@
+﻿var app = (function ($) {
+    var sum = function (a, b) {
+        return a + b;
+    };
+
+    var fade = function (duration) {
+        $('#div1').fadeOut(duration);
+    };
+
+    var fade2 = function (duration, callback) {
+        $('#div1').fadeOut(duration, callback);
+    };
+
+    return {
+        sum: sum,
+        fade: fade,
+        fade2: fade2
+    };
+})($);
