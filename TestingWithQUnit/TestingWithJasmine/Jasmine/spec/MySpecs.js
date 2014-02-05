cab6dd3d404a11b11b46f99a34a85e05ecc498c6
jain208 <abhi2000@gmail.com>
2014-02-05 12:04:03 +0000

    Added Sinon Spies tests

--- TestingWithQUnit/TestingWithJasmine/Jasmine/spec/MySpecs.js
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/spec/MySpecs.js
@@ -156,7 +156,27 @@ describe("App", function () {
                 mySUT.callCallBack(spy);
 
                 expect(spy.called).toBe(true);
-            })
+            });
+
+            it("should call a real implementation if given a real function to spy on", function () {
+                var spy = sinon.spy(realCallback);
+                var returnValue = mySUT.callCallBackWithReturnValue(spy);
+
+                expect(spy.called).toBe(true);
+                expect(returnValue).toBe(4);
+
+            });
+
+
+            it("should spy on a method of an object", function () {
+                var spy = sinon.spy(myDep, 'someMethod');
+                var returnValue = mySUT.callDependency(myDep);
+
+                expect(spy.called).toBe(true);
+                expect(returnValue).toBe(10);
+
+            });
+
         })
     })
 });
--- TestingWithQUnit/TestingWithJasmine/Jasmine/src/App2.js
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/src/App2.js
@@ -1,5 +1,24 @@
 ﻿var mySUT = {
     callCallBack: function (cb) {
         cb();
+    },
+
+    callCallBackWithReturnValue: function (cb) {
+        return cb();
+    },
+
+    callDependency: function (dep) {
+        return dep.someMethod();
     }
+
+}
+
+var myDep = {
+    someMethod : function() {
+        return 10;
+    }
+}
+
+function realCallback() {
+    return 4;
 }
