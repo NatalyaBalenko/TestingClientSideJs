4713ccfd96d44a96100837b9fcc03f2beb00f55c
jain208 <abhi2000@gmail.com>
2014-02-06 09:11:34 +0000

    Added Sinon mocks tests

--- TestingWithQUnit/TestingWithJasmine/Jasmine/spec/MySpecs.js
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/spec/MySpecs.js
@@ -193,5 +193,43 @@ describe("App", function () {
                 sinon.assert.called(spy);
             })
         });
+
+        describe("Stubs", function () {
+
+            describe("Combat attack", function () {
+                it("should damage the defender if the hit is successful", function () {
+                    var combat = new Combat();
+                    var defender = sinon.stub(new Character());
+                    var attacker = sinon.stub(new Character());
+                    attacker.damage = 5;
+                    attacker.calculateHit.returns(true);
+
+                    combat.attack(attacker, defender);
+
+                    expect(defender.takeDamage.called).toBe(true);
+                    expect(defender.takeDamage.getCall(0).calledWith(5)).toBe(true);
+                })
+            });
+
+        });
+
+        describe("Mocks", function () {
+            describe("Combat attack", function () {
+                it("should damage the defender if the hit is successful", function () {
+                    var combat = new Combat();
+                    var defender = new Character();
+                    var mockDefender = sinon.mock(defender);
+                    var expectation = mockDefender.expects("takeDamage").once().withArgs(5);
+
+                    var attacker = sinon.stub(new Character());
+                    attacker.damage = 5;
+                    attacker.calculateHit.returns(true);
+
+                    combat.attack(attacker, defender);
+
+                    expectation.verify();
+                })
+            });
+        });
     })
 });
--- TestingWithQUnit/TestingWithJasmine/Jasmine/src/App2.js
+++ TestingWithQUnit/TestingWithJasmine/Jasmine/src/App2.js
@@ -22,3 +22,21 @@ var myDep = {
 function realCallback() {
     return 4;
 }
+
+
+var Combat = function () {
+
+};
+Combat.prototype.attack = function (attacker, defender) {
+    if (attacker.calculateHit(defender))
+        defender.takeDamage(attacker.damage);
+}
+
+var Character = function () {
+
+};
+Character.prototype.calculateHit = function () {
+};
+
+Character.prototype.takeDamage = function () {
+};
