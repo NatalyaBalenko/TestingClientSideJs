f357501a6268c2ca8efc642edc9f752111035b76
jain208 <abhi2000@gmail.com>
2014-02-07 11:57:13 +0000

    Running tests with karma

---
+++ TestingWithQUnit/TestingWithKarma/Scripts/App/myApp.js
@@ -0,0 +1,23 @@
+﻿var app = (function () {
+    var sum = function (a, b) {
+
+        if (a === undefined || b === undefined)
+            return undefined;
+
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            return undefined;
+        }
+
+        var result = a + b;
+        return a + b;
+    };
+
+    var divide = function (a, b) {
+        return a / b;
+    };
+
+    return {
+        sum: sum,
+        divide: divide
+    };
+})();
---
+++ TestingWithQUnit/TestingWithKarma/Scripts/Tests/MySpecs.js
@@ -0,0 +1,24 @@
+﻿describe("App", function () {
+    describe("sum", function () {
+        it('should get sum of 2 numbers', function () {
+            var result = app.sum(3, 4);
+            expect(result).toBe(7);
+        });
+
+        it('should return undefined if undefined passed', function () {
+            var result = app.sum(3, undefined);
+            expect(result).toBe(undefined);
+        });
+
+        it('should throw an error if anything other than a number is passed', function () {
+            var result = app.sum(3, "a");
+            expect(result).toBe(undefined);
+        });
+
+        //it('should throw an error if more or less than 2 parameterrs are passed', function () {
+        //    var result = app.sum(3, 2, 4);
+        //    expect(app.sum).toThrow();
+        //});
+
+    });
+});
